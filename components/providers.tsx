d2c221405c2c836f253b4b4dfd32bc144f6cce35
Ali Geramy <ali@softxinnovations.ca>
2025-05-16 19:16:57 -0400

    add changes

--- components/providers.tsx
+++ components/providers.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
-      <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
         {children}
       </ThemeProvider>
     </SessionProvider>
