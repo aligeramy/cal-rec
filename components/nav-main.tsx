e5c589072d773fe477315b9dea018847a3efd8a3
Ali Geramy <ali@softxinnovations.ca>
2025-05-26 16:02:25 -0400

    add changes

--- components/nav-main.tsx
+++ components/nav-main.tsx
@@ -38,7 +38,7 @@ export function NavMain({
 
   return (
     <SidebarGroup>
-      <SidebarGroupLabel>Platform</SidebarGroupLabel>
+      <SidebarGroupLabel>Dashboard</SidebarGroupLabel>
       <SidebarMenu>
         {items.map((item) => (
           <Collapsible
